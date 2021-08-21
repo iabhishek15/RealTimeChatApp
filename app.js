34a6ff94e00d07403f048faa9bfda4c79abbf68f
iabhishek15 <158singhabhi@gmail.com>
2021-08-21 11:22:23 +0530

    new change

--- app.js
+++ app.js
@@ -1,225 +1,234 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bcrypt = require('bcrypt');
-var session = require('express-session');
-var passport = require('passport');
-var methodOverride = require('method-override');
-var flash = require('express-flash');
-const multer = require('multer');
-const helpers = require('./helpers');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bcrypt = require("bcrypt");
+var session = require("express-session");
+var passport = require("passport");
+var methodOverride = require("method-override");
+var flash = require("express-flash");
+const multer = require("multer");
+const helpers = require("./helpers");
 
 //const upload = multer({dest: __dirname + '/uploads/images'});
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'uploads/');
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
   },
 
   // By default, multer removes file extensions so let's add them back
-  filename: function(req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
-
-
-
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/chattingdb',{
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/chattingdb", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-require('./models');
+require("./models");
 
 //getting the user model
-var User = mongoose.model('User');
-var Message = mongoose.model('Message');
+var User = mongoose.model("User");
+var Message = mongoose.model("Message");
 
-//passport Initialization 
-var initializePassport = require('./passportValidate.js');
-const { render } = require('ejs');
-const { doesNotMatch } = require('assert');
+//passport Initialization
+var initializePassport = require("./passportValidate.js");
+const { render } = require("ejs");
+const { doesNotMatch } = require("assert");
 initializePassport(User);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(session({
-  secret : 'dfnsnslnfslnfsdlnfssdsds',
-  resave : false,
-  saveUninitialized : false
-}));
+app.use(
+  session({
+    secret: "dfnsnslnfslnfsdlnfssdsds",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride('_method'));
-app.use('/uploads', express.static('uploads'));
+app.use(methodOverride("_method"));
+app.use("/uploads", express.static("uploads"));
 
-app.delete('/logout', function (req, res, next){ 
+app.delete("/logout", function (req, res, next) {
   req.logOut();
-  res.redirect('/');
-}); 
+  res.redirect("/");
+});
 
-
-app.get('/', IsAuthenticated, function (req, res, next) {
-  User.findOne({
-    username : req.user.username
-    }, function (err, user) {
+app.get("/", IsAuthenticated, function (req, res, next) {
+  User.findOne(
+    {
+      username: req.user.username,
+    },
+    function (err, user) {
       if (err) {
         return next(err);
       }
       let userFriendRequest = [];
       for (let i = 0; i < user.friend_request.length; ++i) {
-        User.findOne({
-          username : user.friend_request[i]
-        }, function (err, new_user) {
-          if (err) {
-            return next(err);
-          }
-          //console.log(user.friend_request[i], new_user.image);
-          userFriendRequest.push({
-            user : user.friend_request[i],
-            user_image : new_user.image
-          })
+        const newUser = User.findOne({
+          username: user.friend_request[i],
         });
+        //console.log(newUser.user);
+        userFriendRequest.push({
+          user: user.username,
+          image: newUser.image,
+        });
+        console.log("user friend request", userFriendRequest);
       }
-      //console.log(userFriendRequest);
-      return res.render('chat', {
-        username : req.user.username,
-        friends : user.friends,
-        'profileImage' : user.image,
-        "userFriendRequest" : userFriendRequest
+      console.log(
+        `friend request recieved by ${req.user.username} is`,
+        userFriendRequest
+      );
+      return res.render("chat", {
+        username: req.user.username,
+        friends: user.friends,
+        profileImage: user.image,
+        userFriendRequest: userFriendRequest,
       });
-  });
+    }
+  );
 });
 
-app.get('/login', IsNotAuthenticated, function (req, res, next) {
-  res.render('login');
+app.get("/login", IsNotAuthenticated, function (req, res, next) {
+  res.render("login");
 });
 
-app.get('/signup', IsNotAuthenticated, function (req, res, next) {
-  res.render('signup');
+app.get("/signup", IsNotAuthenticated, function (req, res, next) {
+  res.render("signup");
 });
 
-app.post('/upload-profile-pic', (req, res) => {
+app.post("/upload-profile-pic", (req, res) => {
   // 'profile_pic' is the name of our file input field in the HTML form
-  let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
+  let upload = multer({
+    storage: storage,
+    fileFilter: helpers.imageFilter,
+  }).single("profile_pic");
 
-  upload(req, res, function(err) {
-      // req.file contains information of uploaded file
-      // req.body contains information of text fields, if there were any
+  upload(req, res, function (err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
 
-      if (req.fileValidationError) {
-          return res.send(req.fileValidationError);
-      }
-      else if (!req.file) {
-          return res.send('Please select an image to upload');
-      }
-      else if (err instanceof multer.MulterError) {
-          return res.send(err);
-      }
-      else if (err) {
-          return res.send(err);
-      }
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("Please select an image to upload");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
 
-      // Display uploaded image for user validation
-      //console.log("full path ", req.file.path);
-      
-      //storing the image in the database
-      User.updateOne({
-        username : req.user.username
-      }, { 
-        $set : { 
-          image : req.file.path
-        }
-      }, {
-        "upsert": false
-      }).then((result) => {
+    // Display uploaded image for user validation
+    //console.log("full path ", req.file.path);
+
+    //storing the image in the database
+    User.updateOne(
+      {
+        username: req.user.username,
+      },
+      {
+        $set: {
+          image: req.file.path,
+        },
+      },
+      {
+        upsert: false,
+      }
+    )
+      .then((result) => {
         console.log(result);
-      }).catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
       });
 
-      res.redirect('/');
+    res.redirect("/");
   });
 });
 
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
-}));
-
-app.post('/signup', function (req, res, next) {
+app.post("/signup", function (req, res, next) {
   //console.log(req.body);
   if (req.body.password != req.body.confirmpassword) {
-    req.flash('error', 'password do not match');
-    return res.redirect('/signup');
+    req.flash("error", "password do not match");
+    return res.redirect("/signup");
   }
-  User.findOne({
-    username:req.body.username
-  }, function (err, user) {
-    if (err) return next(err);
-    if (user) {
-      req.flash('error', 'Username already exits');
-      return res.redirect('/signup');
-    }
-    let newUser = new User({
+  User.findOne(
+    {
       username: req.body.username,
-      passwordHash: bcrypt.hashSync(req.body.password, 10)
-    });
-    newUser.save();
-    return res.redirect('/');
-  })
+    },
+    function (err, user) {
+      if (err) return next(err);
+      if (user) {
+        req.flash("error", "Username already exits");
+        return res.redirect("/signup");
+      }
+      let newUser = new User({
+        username: req.body.username,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+      });
+      newUser.save();
+      return res.redirect("/");
+    }
+  );
 });
-
 
 function IsAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 function IsNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   next();
 }
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- chat.js
+++ chat.js
@@ -1,14 +1,14 @@
 function chatting(server) {
-  const socketio = require('socket.io');
+  const socketio = require("socket.io");
   const io = socketio(server);
-  const mongoose = require('mongoose');
-  const User = mongoose.model('User');
-  const Message = mongoose.model('Message');
+  const mongoose = require("mongoose");
+  const User = mongoose.model("User");
+  const Message = mongoose.model("Message");
 
-  io.on('connection', socket => {
-    console.log('A user has been connected');
+  io.on("connection", (socket) => {
+    console.log("A user has been connected");
 
-    socket.on('message', function (message) {
+    socket.on("message", function (message) {
       //console.log(message);
       var personOne = message.sender;
       var personTwo = message.receiver;
@@ -16,64 +16,70 @@ function chatting(server) {
         personTwo = message.sender;
         personOne = message.receiver;
       }
-      Message.findOne({
-        personOne : personOne,
-        personTwo : personTwo        
-      }, function (err, mess){
-        if (err) {
-          console.log(err);
-          return ;
+      Message.findOne(
+        {
+          personOne: personOne,
+          personTwo: personTwo,
+        },
+        function (err, mess) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (!mess) {
+            createMessage(Message, message, personOne, personTwo);
+            return;
+          }
+          let texts = mess.text;
+          let date = new Date();
+          let time =
+            date.getDate() + "/" + date.getDay() + "/" + date.getFullYear();
+          texts.push({
+            sender: message.sender,
+            receiver: message.receiver,
+            text: message.text,
+            time: time,
+          });
+          updateMessage(Message, mess._id, texts);
         }
-        if (!mess) {
-          createMessage(Message, message, personOne, personTwo);
-          return ;
-        }
-        let texts = mess.text;  
-        let date = new Date();
-        let time = date.getDate() + "/" + date.getDay() + "/" + date.getFullYear()
-        texts.push({
-          sender : message.sender,
-          receiver : message.receiver,
-          text : message.text,
-          time : time
-        });
-        updateMessage(Message, mess._id, texts);
-      });
-      socket.broadcast.emit('otherMessage', message.text);
+      );
+      socket.broadcast.emit("otherMessage", message.text);
     });
 
-    socket.on('searchFriend', function (user) {
-      User.findOne({
-        username : user
-      }, function (err, user) {
-        if (err) {
-          console.log(err);
-          return ;
+    socket.on("searchFriend", function (user) {
+      User.findOne(
+        {
+          username: user,
+        },
+        function (err, user) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (!user) {
+            socket.emit("FriendExists", false);
+            return;
+          }
+          socket.emit("FriendExists", true);
         }
-        if (!user) {
-          socket.emit('FriendExists', false);
-          return ;
-        }
-        socket.emit('FriendExists', true);
-      });
+      );
     });
     //Receving the user to send the friend request
-    socket.on('friendRequest', (user) => {
+    socket.on("friendRequest", (user) => {
       //saving the friend request in the database
       add_friend_request_to_database(User, user.me, user.friend);
-      socket.broadcast.emit('AddFriendRequest', {
-        "receiver" : user.friend,
-        "sender" : user.me
+      socket.broadcast.emit("AddFriendRequest", {
+        receiver: user.friend,
+        sender: user.me,
       });
     });
 
-    socket.on('AddFriend', (user) => {
-      AddFriend(User, user.user1,user.user2);
-      AddFriend(User, user.user2,user.user1);
+    socket.on("AddFriend", (user) => {
+      AddFriend(User, user.user1, user.user2);
+      AddFriend(User, user.user2, user.user1);
     });
-    
-    socket.on('friendTalkHistory', user => {
-      //console.log(user.user1, user.user2);
+
+    socket.on("friendTalkHistory", (user) => {
       var personOne = user.user1;
       var personTwo = user.user2;
       if (user.user1 > user.user2) {
@@ -81,110 +87,147 @@ function chatting(server) {
         personOne = user.user2;
       }
       console.log(personOne, personTwo);
-      Message.findOne({
-        personOne : personOne,
-        personTwo : personTwo
-      }, function (err, data) {
-        if (err) {
-          console.log(err);
-          return ;
+      Message.findOne(
+        {
+          personOne: personOne,
+          personTwo: personTwo,
+        },
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (data == null) {
+            return;
+          }
+          socket.emit("userChatHistory", data.text);
         }
-        if (data == null) {
-          return ;
-        }
-       socket.emit('userChatHistory', data.text);
-      });
+      );
     });
-
   });
 }
 module.exports = chatting;
 
-
-
 function AddFriend(User, user1, user2) {
-  User.findOne({
-    username : user1
-  }, function (err, user) {
-    if (err) {
-      console.log(err);
-      return ;
-    }
-    user.friends.push(user2);
-    console.log(user.friends);
-    User.updateOne({
-      _id : user._id
-    }, { 
-      $set : { 
-        friends : user.friends 
+  User.findOne(
+    {
+      username: user1,
+    },
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return;
       }
-    }, {
-      "upsert": false
-    }).then((result) => {
-      console.log(result);
-    }).catch((err) => {
-      console.log(err);
-    });
-  });
+      user.friends.push(user2);
+      //printing the user.friends
+      console.log(user.friends);
+      User.updateOne(
+        {
+          _id: user._id,
+        },
+        {
+          $set: {
+            friends: user.friends,
+          },
+        },
+        {
+          upsert: false,
+        }
+      )
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  );
 }
 
-//creating the message  
+//creating the message
 function createMessage(Message, message, personOne, personTwo) {
-  const new_message = new Message;
+  const new_message = new Message();
   new_message.personOne = personOne;
   new_message.personTwo = personTwo;
   let date = new Date();
-  let time = date.getDate() + "/" + date.getDay() + "/" + date.getFullYear()
+  let time = date.getDate() + "/" + date.getDay() + "/" + date.getFullYear();
   new_message.text.push({
-    sender : message.sender,
-    receiver : message.receiver,
-    text : message.text,
-    time : time
+    sender: message.sender,
+    receiver: message.receiver,
+    text: message.text,
+    time: time,
   });
   new_message.save();
 }
 //updating the message
 function updateMessage(Message, id, texts) {
-  Message.updateOne({
-    _id : id
-  }, { 
-    $set : { 
-      text : texts 
+  Message.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        text: texts,
+      },
+    },
+    {
+      upsert: false,
     }
-  }, {
-    "upsert": false
-  }).then((result) => {
-    console.log(result);
-  }).catch((err) => {
-    console.log(err);
-  });
+  )
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 //adding the friend request to database
 function add_friend_request_to_database(User, me, friend) {
-  User.findOne({
-    username : friend
-  }, function (err, user) {
-    if (err) {
-      console.log(err);
-      return ;
-    }
-    user.friend_request.push(me);
-    console.log("friend_request", user.friend_request);
-    User.updateOne({
-      _id : user._id
-    }, { 
-      $set : { 
-        friend_request : user.friend_request 
+  User.findOne(
+    {
+      username: friend,
+    },
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return;
       }
-    }, {
-      "upsert": false
-    }).then((result) => {
-      console.log(result);
-    }).catch((err) => {
-      console.log(err);
-    });
-  });
+      let newFriendRequest = [...user.friend_request];
+      User.findOne(
+        {
+          username: me,
+        },
+        function (err, newUser) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          newFriendRequest.push({
+            friend: me,
+            image: newUser.image,
+          });
+          User.updateOne(
+            {
+              _id: user._id,
+            },
+            {
+              $set: {
+                friend_request: newFriendRequest,
+              },
+            },
+            {
+              upsert: false,
+            }
+          )
+            .then((result) => {
+              console.log(result);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      );
+    }
+  );
 }
-
-
